036cd9eca0717ec6b01268b41ee062870e8dfb51
JeffChiu <jchiu@KCC.local>
2016-04-26 18:45:38 -0700

    MemoryGame push commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Jeff Chiu on 3/28/16.
+//  Copyright © 2016 Jeff Chiu. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryGame
+//
+//  Created by Jeff Chiu on 3/28/16.
+//  Copyright © 2016 Jeff Chiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
